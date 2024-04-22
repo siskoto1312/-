0fa2b55cb58e37fb072ee7f2d45c0da74ac7d6ab
siskoto1312 <146094642+siskoto1312@users.noreply.github.com>
2024-04-22 09:47:54 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,24 @@
-﻿namespace sti
+using System;
+using System.Linq;
+
+class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        var firstList = Console.ReadLine().Split(' ').Select(s => double.Parse(s)).ToList();
+        var secondList = Console.ReadLine().Split(' ').Select(s => double.Parse(s)).ToList();
+
+        // Пересечение
+        Console.WriteLine("Section = " + string.Join(", ", firstList.Intersect(secondList)));
+
+        // Объединение
+        Console.WriteLine("Union = " + string.Join(", ", firstList.Union(secondList)));
+
+        // Разница
+        Console.WriteLine("Difference = " + string.Join(", ", firstList.Except(secondList)));
+
+        // Сложение
+        Console.WriteLine("Addition = " + string.Join(", ", firstList.Concat(secondList)));
     }
 }
+
